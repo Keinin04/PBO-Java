5d472b4e447494e3fcd0275d7ac79bb4c67308a6
Rifqy P <60067877+Keinin04@users.noreply.github.com>
2023-06-11 10:58:58 +0700

    new

---
+++ perpustakaan/src/main/java/com/perpustakaan/Perpustakaan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.perpustakaan;
+
+/**
+ *
+ * @author rifky
+ */
+public class Perpustakaan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
